6bf7998e9e30310b93ea38dfd65aa86a428a16b7
bunny <444543565@qq.com>
2019-11-13 18:03:43 +0800

    regular backup

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -8,22 +8,22 @@ class ListNode:
         self.next = None
 
 
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         l3 = None
         listNode3 = l3
         mark = 0
         while 1:
+            add = 0
             if l1 is None and l2 is None:
                 if mark == 1:
                     listNode3.next = ListNode(1)
                 return l3
-            if l1 is None:
-                add = l2.val
-            elif l2 is None:
-                add = l1.val
-            else:
-                add = l1.val+l2.val
+            if l1:
+                add += l1.val
+            if l2:
+                add += l2.val
 
             if mark == 1:  # 判断前一位是否有进位  (5)+(5)
                 add += 1  # 有进位,和加 1
@@ -33,16 +33,16 @@ class Solution:
                 mark = 1  # 设置进位标记为 1
                 add -= 10  # 更改为 1 位数
 
-            if l1 is not None or l2 is not None or mark == 0:
-                if l3 is None:
-                    l3 = ListNode(add)
-                    listNode3 = l3
-                else:
-                    listNode3.next = ListNode(add)
-                    listNode3 = listNode3.next
-            l1 = l1.next if l1 else l1
-            l2 = l2.next if l2 else l2
-
+            if l3 is None:
+                l3 = ListNode(add)
+                listNode3 = l3
+            else:
+                listNode3.next = ListNode(add)
+                listNode3 = listNode3.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 def main():
     sol = Solution()
     listNode1 = None
